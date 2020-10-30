aed12280422d596ecfbcb77a72411fc4e7e63765
ViniciusCrisol <vinicius.crisol@hotmail.com>
2020-10-30 20:56:37 -0300

    Add JS for menu

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,7 +1,41 @@
 const hamburgerButton = this.document.getElementById('hamburger-button');
+const mobileMenu = this.document.getElementById('mobile-menu');
 
-function handleTogleMenuVisibility() {
-  console.log('bom dia');
+let menuIsActive = false;
+
+function addClassActiveToMenu() {
+  mobileMenu.classList.add('active');
 }
 
-hamburgerButton.addEventListener('click', () => handleTogleMenuVisibility());
+function removeClassActiveToMenu() {
+  mobileMenu.classList.remove('active');
+}
+
+function closeMenu() {
+  removeClassActiveToMenu();
+  menuIsActive = false;
+}
+
+function toggleMenuVisibility() {
+  if (menuIsActive) {
+    closeMenu();
+    return;
+  }
+
+  menuIsActive = true;
+  addClassActiveToMenu();
+}
+
+function getWindowSize() {
+  return this.screen.availWidth;
+}
+
+function removeClassActiveByWindowSize() {
+  const windowSize = getWindowSize();
+  const WIDTH_TO_CHANGE_LAYOUT = 640;
+
+  if (windowSize >= WIDTH_TO_CHANGE_LAYOUT) closeMenu();
+}
+
+hamburgerButton.addEventListener('click', () => toggleMenuVisibility());
+this.window.addEventListener('resize', () => removeClassActiveByWindowSize());
